efadc739e605240f9e388bc61796c74821b0fd41
jereimer <jens@reimer-warendorf.de>
2017-05-16 12:03:35 +0200

    HeaderListElement

--- listElement.h
+++ listElement.h
@@ -1,1 +1,32 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// File: listElement.h
+//
+// Ingenieurorientierte Programmierung
+// Dr. Alexander Kling
+//
+// Carsten Bevermann
+// Christoph Geitner
+// Jens Reimer
+// Marc Töpker
+//
+///////////////////////////////////////////////////////////////////////////////
 
+#ifndef LISTELEMENT_H
+#define LISTELEMENT_H
+
+#include <string>
+#include <iostream>
+
+class listElement
+{
+    std::string content;
+    public:
+    void operator= (listElement);
+    void swapContent (listElement);
+    friend ostream& operator<< (ostream& os, const listElement& element);
+    void setContent(std::string);
+    std::string getContent();
+
+};
+#endif // LISTELEMENT_H
