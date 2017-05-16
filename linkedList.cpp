82d83b83719e8f0f8651c241f8289ba2d97d1245
MarcToepker <marc.toepker@hs-osnabrueck.de>
2017-05-16 12:03:26 +0200

    Funktionen erstellt

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,1 +1,39 @@
+#ifndef LINKEDLIST_CPP
+#define LINKEDLIST_CPP
 
+#include "listElement.h"
+#include "linkedList.h"
+#include <iostream>
+
+using namespace std;
+
+linkedList::linkedList()
+{
+    new listElement
+}
+
+linkedList::linkedList( linkedList )
+{
+}
+
+linkedList::append( string s )
+{
+}
+
+linkedList::insert( int i, string s )
+{
+}
+
+linkedList::sort()
+{
+}
+
+linkedList::deleteElement( int i )
+{
+}
+
+linkedList::deleteElement( string s )
+
+ostream& operator<< ( ostream& os, const rational& ratBruch )
+{
+}
--- linkedList.h
+++ linkedList.h
@@ -1,1 +1,50 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// File: linkedList.h
+//
+// Ingenieurorientierte Programmierung
+// Dr. Alexander Kling
+//
+// Carsten Bevermann
+// Christoph Geitner
+// Jens Reimer
+// Marc Töpker
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include <iostream>
+using namespace std;
+class linkedList
+{
+private:
+    /* set functions */
+    void setStartElement( listElement* );
+    void setActualElement ( listElement* );
+
+    /* get functions */
+    listElement getStartElement (  );
+    listElement getActualElement (  );
+
+
+public:
+    /* Konstruktoren */
+    linkedList();               //initialize LinkedList
+    linkedList("");             //initialize LinkedList with content
+    linkedList(linkedList);     //copyConstructor for LinkedList
+    /* Schnittstellenfkt */
+    void sort(  );              // sortierung asc
+    void append( string );      // element an das ende der liste anhängen
+    void insert( int, string ); // element an einer bestimmten position einfügen
+    void deleteElement( int )   // soll ein element an einer stelle löschen
+    void deleteElement( string )// soll ein element mit dem Content löschen
+    /* Überladungsfkt */
+    friend ostream& operator<< (ostream& os, const rational& ratBruch);
+};
+
+#endif // linkedList_H
+
+/////////////////// End of File <linkedList.h> //////////////////////////////////
 
