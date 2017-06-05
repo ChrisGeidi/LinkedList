0e2bf17521541a8d32a03bebe6f678d1fecf61a1
MarcToepker <marc.toepker@hs-osnabrueck.de>
2017-06-05 14:04:26 +0200

    Datei mit Anforderungen hinzugefügt

--- testApp.cpp
+++ testApp.cpp
@@ -2,7 +2,6 @@
 #include "linkedList.cpp"
 #include <iostream>
 #include <string>
-#include <typeinfo>
 
 
 using namespace std;
@@ -11,21 +10,21 @@ int main()
 {
     // vars
     ///*string list*/
-    /*linkedList<string>* myList = new linkedList<string>;    //für string lists
+    /*linkedList<string>* myList = new linkedList<string>;
     string  Test1 = "Jens";
     string  Test2 = "Christoph";
     string  Test3 = "Carsten";
     string  Test4 = "Marc";*/
 
     ///*integer list*/
-    /*linkedList<int>* myList = new linkedList<int>;          // für int lists
+    /*linkedList<int>* myList = new linkedList<int>;
     int     Test1 = 500;
     int     Test2 = 152;
     int     Test3 = 348;
     int     Test4 = 111;*/
 
     ///*double list*/
-    linkedList<double>* myList = new linkedList<double>;          // für double lists
+    linkedList<double>* myList = new linkedList<double>;
     double     Test1 = 768.55;
     double     Test2 = 152.62;
     double     Test3 = 348.24;
