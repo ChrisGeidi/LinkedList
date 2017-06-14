ed0b0147c3bb630b441009e608e885d5278944ec
Christoph <christoph.geitner@gmail.com>
2017-06-14 21:22:41 +0200

    listElement.h
    - nichtmehr benötigte Sachen gelöscht
    - Kommentare hinzugefügt

--- listElement.h
+++ listElement.h
@@ -17,32 +17,52 @@
 
 #include <iostream>
 
-template <class T>
-class listElement;
-
-template <class T>
-std::ostream& operator<<(std::ostream& os,  listElement<T>& liste);
-
 template <class T> class listElement
 {
+    // Speichert den Inhalt des Listenelementes
     T content;
+
+    // Speichert die Adresse des naechsten Elementes in der Liste
     listElement* ptrNextElement;
 
 public:
+
+    // Custom-Constructor
     listElement(  );
+
+    // Custom-Constructor
+    //
+    // Parameter 1: Inhalt des zu erstellenden Elementes
     listElement( T );
+
+    // Kopier-Konstruktor
+    //
+    // Paramter 1: Adresse des Quellelementes
     listElement( listElement& );
+
+    // Setzt Inhalt des Elementes
     void setContent( T );
+
+    // Setzt die Adresse des naechsten Elementes in der Liste
     void setNextElement( listElement* );
 
+    // Gibt den Inhalt des Elementes zurueck
     T getContent(  );
+
+    // Gibt die Adresse des naechsten Elementes zurueck
     listElement* getNextElement(  );
 
-    void operator= ( listElement* );
-    //friend std::ostream& operator<< <>( std::ostream& os, listElement<T>& element);
-
+    // Tauscht den Inhalt zweier Elemente
+    //
+    // Parameter 1: Adresse des zu tauschenden Elementes
     void swapContent( listElement<T>* );
+
+    // Ueberladungsfunktion des =-Operators
+    void operator= ( listElement* );
 };
+
+template <class T>
+std::ostream& operator<<(std::ostream& os,  listElement<T>& liste);
 
 #endif // LISTELEMENT_H
 
