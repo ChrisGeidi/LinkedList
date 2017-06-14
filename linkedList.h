3a2df9464c407622f607cf2b17ebdcb74abfcf5f
Christoph <christoph.geitner@gmail.com>
2017-06-14 19:27:00 +0200

    linkedList.h
    - Kommentare zu Funktionen hinzugefügt
    - Auskommentieres und überflüssiges rausgelöscht

--- linkedList.h
+++ linkedList.h
@@ -1,4 +1,3 @@
-
 ///////////////////////////////////////////////////////////////////////////////
 //
 // File: linkedList.h
@@ -17,48 +16,88 @@
 #define LINKEDLIST_H
 
 #include <iostream>
-#include "listElement.h"
 #include <stdio.h>
-
-template <class T>
-class linkedList;
-
-template <class T>
-std::ostream& operator<<(std::ostream& os,  linkedList<T>& liste);
+#include "listElement.h"
 
 template <class T> class linkedList
 {
 private:
-    int length;                 // length of list
-    listElement<T>* start;      // beginnendes element
-    listElement<T>* active;     // akutelles bzw. zuletzt eingefügtes element
 
+    // Laenge der Liste
+    int length;
+
+    // Beginnendes Element
+    listElement<T>* start;
+
+    // Akutelles bzw. zuletzt eingefuegtes Element
+    listElement<T>* active;
+
+    // Setzt ein Element als Start-Element
+    //
+    // Parameter 1: Adresse des zu setzenden Elementes
     void setStartElement( listElement<T>* );
+
+    // Setzt ein Element als Aktives-Element
+    //
+    // Parameter 1: Adresse des zu setzenden Elementes
     void setActiveElement ( listElement<T>* );
+
 public:
+
+    // Custom-Constructor
     linkedList(  );
+
+    // Custom-Constructor für Datentype T
     linkedList( T );
+
+    // Custom-Kopierkonstruktor
     linkedList( linkedList& );
 
+    // Destruktor
     ~linkedList();
 
+    // Sortiert die Liste nach ASCII-Nummer, beginnend beim ersten Zeichen
     void sortList(  );
-    void append( T );
-    void insertElement( int, T );
-    void deleteElement( int );
-    void removeElement( T );
-    //void printList (  );
 
+    // Fuegt der Liste ein Element an
+    //
+    // Parameter 1: Inhalt, der angehaengt werden soll
+    void append( T );
+
+    // Fuegt an gegebener Stelle der Liste ein Element des Datentyps T ein
+    //
+    // Parameter 1: Steller, an der ein Element eingefuegt werden soll
+    // Parameter 2: Inhalt, der eingefuegt werden soll
+    void insertElement( int, T );
+
+    // Entfertn ein Element aus der Liste
+    //
+    // Parameter 1: Stelle des Elementes, das entfernt werden soll
+    void deleteElement( int );
+
+    // Entfernt ein Element aus der Liste
+    //
+    // Parameter 1: Inhalt des Elementes, das entfernt werden soll
+    void removeElement( T );
+
+    // Konvertiert Wert zu einem String
+    //
+    // Parameter 1: Adresse des Wertes, der konvertiert werden soll
     string to_string(T const& value);
 
+    // Gibt Adresse des ersten Elementes zurueck
     listElement<T>* getStartElement (  );
+
+    // Gibt Adresse des aktiven Elementes zurueck
     listElement<T>* getActiveElement (  );
 
+    // Gibt aktuelle Laenge der Liste zurueck
     int getLength();
-    //listElement* getAddressOfElementsNumber(int);   // gibt Adresse eines numerierten Elementes zurück
-    /* Überladungsfkt */
-    //friend std::ostream& operator<< <>(std::ostream& os,  listElement<T>& liste);
 };
+
+// Ueberladungsfunktion des <<-Operators
+template <class T>
+std::ostream& operator<<(std::ostream& os,  linkedList<T>& liste);
 
 #endif // linkedList_H
 
